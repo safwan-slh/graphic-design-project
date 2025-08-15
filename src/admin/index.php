e979dda7c3ab37c465fb2019f149603c639cb0b6
Safwan <safwansalaeh464@gmail.com>
2025-08-16 00:25:13 +0700

    add sidebar component and update admin dashboard layout

--- src/admin/index.php
+++ src/admin/index.php
@@ -1,11 +1,20 @@
+<?php
+require '../auth/auth.php';
+requireRole('admin'); // ให้เข้าหน้านี้ได้เฉพาะ admin
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>Dashboard</title>
+          <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet" />
+          <link href="../../dist/output.css" rel="stylesheet" />
 </head>
 <body>
-          
+          <?php include '../includes/sidebar.php'; ?>
+          <div class=" ml-64 p-8 flex-1 flex items-center justify-center min-h-screen px-4">
+                    <h2>Welcome Admin, <?php echo $_SESSION['fullname']; ?></h2>
+          </div>
 </body>
 </html>
