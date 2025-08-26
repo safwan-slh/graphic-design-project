93f1182aef980265a3a89d1f16537a1770c87eee
Safwan <safwansalaeh464@gmail.com>
2025-08-26 15:28:30 +0700

    add insert portfolio page and portfolio list page

---
+++ src/client/portfolios.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Portfolios</title>
+          <link href="../../dist/output.css" rel="stylesheet" />
+</head>
+
+<body>
+          <?php
+          include __DIR__ . '/../includes/navbar.php';
+          ?>
+</body>
+
+</html>
--- src/client/services.php
+++ src/client/services.php
@@ -185,8 +185,6 @@ $advantages = [
 
 
 
-    <!-- Footer -->
-    <?php include __DIR__ . '/../includes/footer.php'; ?>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/js/all.min.js"></script>
 </body>
